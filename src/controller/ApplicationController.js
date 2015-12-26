714f23add44e30104e24f32ad45ba5d60d03ec6d
Karl Johnson <karl.rw.johnson@gmail.com>
2015-12-25 20:34:39 -0600

    Implement saving

--- src/controller/ApplicationController.js
+++ src/controller/ApplicationController.js
@@ -3,6 +3,7 @@
 const bindObservers = require('../util/bindObservers');
 const Dom = require('../util/Dom');
 const FileLoader = require('./FileLoader');
+const FileSaver = require('./FileSaver');
 const GUIWindow = require('../util/GUIWindow');
 const Worksheet = require('../model/Worksheet');
 const WorksheetController = require('./WorksheetController');
@@ -13,62 +14,89 @@ class ApplicationController {
     bindObservers(this);
 
     this._fileLoader = new FileLoader();
+    this._fileSaver = new FileSaver();
     this.worksheetController = new WorksheetController(new Worksheet());
 
     this.element = Dom.div({'id': 'app-root', 'class': 'app-root flex-column'},
+      this._fileLoader.element,
+      this._fileSaver.element,
+
       Dom.div({'class': 'toolbar'},
-        this._fileLoader.element,
         Dom.button({
           id: 'newWorksheet',
           class: 'toolbarButton',
           title: 'New Worksheet',
           onclick: () => this.worksheetController.worksheet = new Worksheet()
         }, Dom.img({src: 'img/new.svg'})),
+
         Dom.button({
           id: 'openWorksheet',
           class: 'toolbarButton',
           title: 'Open Worksheet',
           onclick: () => this._fileLoader.prompt()
-            .then(spec => {
-              console.log('got ', spec);
-              this.worksheetController.worksheet = new Worksheet(spec) })
+            .then(contents => {
+              let spec;
+              try {
+                spec = JSON.parse(contents);
+              }
+              catch (e) {
+                throw new Error(
+                  `File ${this.element.value} appears to be corrupt, as there was ` +
+                  `a syntax error while trying to parse it as JSON (${e.message})`
+                );
+              }
+              this.worksheetController.worksheet = new Worksheet(spec);
+            })
             .catch(e => {
               console.error(e);
               GUIWindow.require('alert')(e.message);
             })
         }, Dom.img({src: 'img/open.svg'})),
+
         Dom.button({
           id: 'saveWorksheet',
           class: 'toolbarButton',
           title: 'Save Worksheet',
-          onclick: () => GUIWindow.require('alert')('not implemented'),
+          onclick: () => this._fileSaver
+            .prompt(JSON.stringify(this.worksheetController.worksheet.toJSON()))
+            .catch(e => {
+              console.error(e);
+              GUIWindow.require('alert')(e.message);
+            })
         }, Dom.img({src: 'img/save.svg'})),
+
         Dom.hr(),
+
         this.deleteTable = Dom.button({
           id: 'deleteTable',
           class: 'toolbarButton',
           title: 'Delete Table',
           onclick: () => this.worksheetController.worksheet.removeTable(this.worksheetController.focusedTable.model)
         }, Dom.img({src: 'img/deleteTable.svg'})),
+
         Dom.hr(),
+
         this.insertColumn = Dom.button({
           id: 'insertColumn',
           class: 'toolbarButton',
           title: 'Insert Column',
           onclick: () => this.worksheetController.focusedTable.insertColumn()
         }, Dom.img({src: 'img/insertColumn.svg'})),
+
         this.insertRow = Dom.button({
           id: 'insertRow',
           class: 'toolbarButton',
           title: 'Insert Row',
           onclick: () => this.worksheetController.focusedTable.insertRow()
         }, Dom.img({src: 'img/insertRow.svg'})),
+
         this.deleteColumn = Dom.button({
           id: 'deleteColumn',
           class: 'toolbarButton',
           title: 'Delete Column',
           onclick: () => this.worksheetController.focusedTable.deleteColumn()
         }, Dom.img({src: 'img/deleteColumn.svg'})),
+
         this.deleteRow = Dom.button({
           id: 'deleteRow',
           class: 'toolbarButton',
--- src/controller/FileSaver.js
+++ src/controller/FileSaver.js
@@ -3,9 +3,16 @@
 const Dom = require('../util/Dom');
 const Fn = require('../util/Fn');
 const FileReader = require('../util/GUIWindow').require('FileReader');
+const fs = require('fs');
+
+class JSONOpenException extends Error {}
+class JSONParseException extends Error {}
 
 module.exports =
 class FileLoader {
+
+  get JSONOpenException () { return JSONOpenException; }
+  get JSONParseException () { return JSONParseException; }
 
   constructor () {
     this.element = Dom.div({style: 'display:none;'});
@@ -17,24 +24,15 @@ class FileLoader {
    *     - {FileLoader.JSONParseException} - If the JSON file could not be parsed correctly
    *     - {FileLoader.JSONOpenException} - If the JSON file could not be loaded, e.g. it doesn't exist
    */
-  prompt () {
-    const input = Dom.input({type: 'file'});
+  prompt (contents) {
+    const input = Dom.input({type: 'file', nwsaveas: ''});
     this.element.appendChild(input);
 
     return new Promise((resolve, reject) => {
       input.addEventListener('change', () => {
-        Fn.forEach(Fn.first(input.files), file => {
-          const reader = new FileReader();
-          reader.addEventListener('error', () =>
-            reject(new Error(
-              `File ${this.element.value} could not be opened`
-            ))
-          );
-          reader.addEventListener('load', () => {
-            resolve(reader.result);
-          });
-          reader.readAsText(file);
-        });
+        Fn.forEach(Fn.first(input.files), file =>
+          fs.writeFileSync(file.path, contents)
+        );
       });
       input.click();
       this.element.removeChild(input);
